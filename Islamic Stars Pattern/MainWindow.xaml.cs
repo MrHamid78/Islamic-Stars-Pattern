21df6897f9106b7119ba99aba4a3c6e5afaa8545
MrHamid <hamidrezakouchakzadeh@gmail.com>
2023-08-31 11:30:35 +0330

    Add Button Event + Move Coordinates Class To Class Directory

--- Islamic Stars Pattern/MainWindow.xaml.cs
+++ Islamic Stars Pattern/MainWindow.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
+
 using System.Windows.Shapes;
 
 namespace Islamic_Stars_Pattern
@@ -158,6 +151,11 @@ namespace Islamic_Stars_Pattern
                 return middleOfYAxis;
             else
                 return middleOfYAxis - x;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
