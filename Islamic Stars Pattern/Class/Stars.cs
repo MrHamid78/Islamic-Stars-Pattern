4e742eddabbc4cb841e52c9b3e285af1ecf477d2
MrHamid <hamidrezakouchakzadeh@gmail.com>
2023-09-01 21:59:16 +0330

    Add Stars Basics

---
+++ Islamic Stars Pattern/Class/Stars.cs
@@ -0,0 +1,22 @@
+﻿using Islamic_Stars_Pattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Islamic_Stars_Pattern.Class
+{
+    internal class Stars : Draw, FactoryInterface
+    {
+        public void drawPrimitivePattern()
+        {
+            throw new NotImplementedException();
+        }
+
+        void FactoryInterface.draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
