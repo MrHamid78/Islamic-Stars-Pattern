c117c40c2fca37fa2c5e2344f31535eb89978bd8
MrHamid <hamidrezakouchakzadeh@gmail.com>
2023-09-01 23:25:48 +0330

    Add Filter To Double Values !

--- Islamic Stars Pattern/MainWindow.xaml.cs
+++ Islamic Stars Pattern/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using Islamic_Stars_Pattern.Class;
 using Islamic_Stars_Pattern.Interfaces;
 using System;
-
+using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 using System.Windows.Shapes;
@@ -161,5 +163,52 @@ namespace Islamic_Stars_Pattern
             ScaleLabel.Content = "Scale : " + zoom;
             this.creator();
         }
+
+
+        private bool IsValidDouble(string text)
+        {
+            // Try to parse the text as a double
+            if (double.TryParse(text, out double result))
+            {
+                // Successfully parsed as a double
+                return true;
+            }
+
+            // Check if the text contains a single decimal point
+            if (text.Count(c => c == '.') == 1)
+            {
+                return double.TryParse(text, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out result);
+            }
+
+            // Invalid double
+            return false;
+        }
+        private bool IsNegativeDouble(string text)
+        {
+            if (text == "-")
+            {
+                return true; // Allow a single negative sign
+            }
+
+            if (text.StartsWith("-"))
+            {
+                text = text.Substring(1); // Remove the negative sign
+            }
+
+            return IsValidDouble(text); // Check if the remaining text is a valid double
+        }
+        private void DoubleTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Check if the entered text is a valid double
+            TextBox textBox = sender as TextBox;
+            string newText = textBox.Text.Insert(textBox.CaretIndex, e.Text);
+
+            if (!IsValidDouble(newText) && !IsNegativeDouble(newText) && !e.Text.All(char.IsControl))
+            {
+                e.Handled = true; // Prevent the invalid input from being entered
+            }
+        }
+
+        
     }
 }
